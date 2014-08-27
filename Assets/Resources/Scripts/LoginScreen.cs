cb08978cb4140a34c5c57fda408879de3df04005
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-26 18:00:13 -0600

    Initial commit

---
+++ Assets/Resources/Scripts/LoginScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Resources.Scripts
+{
+    public class LoginScreen : MonoBehaviour
+    {
+        public Text Username;
+        public Text Password;
+
+        public void Login()
+        {
+            // TODO: Validate login info here
+            if (Username.text == "eric" && Password.text == "password")
+            {
+                // TODO: Login functionality here
+            }
+        }
+    }
+}
